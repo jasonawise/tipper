b6aabbe1d6a997da5d2fb9766eb96876f52e5979
Jason A. Wise <jasonandrewwise@gmail.com>
2022-10-27 18:08:13 -0400

    Initial Commit

---
+++ Tipper/TipperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipperApp.swift
+//  Tipper
+//
+//  Created by Jason Wise on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TipperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
